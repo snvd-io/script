5afce1cb050a8e39c0dd6a4bcfb4e4d3d91e1374
Daniel Micay <daniel.micay@grapheneos.org>
2024-04-02 19:02:15 -0400

    April kernel tags

--- manage.sh
+++ manage.sh
@@ -120,45 +120,45 @@ readonly kernels=(
 )
 
 declare -Ar kernel_tags_old=(
-    # March 2024
-    [kernel_build-redbull]=android-14.0.0_r0.58
-    [kernel_msm-redbull]=android-14.0.0_r0.58
-    [kernel_msm-modules_qcacld-redbull]=android-14.0.0_r0.58
-    [kernel_msm-extra-redbull]=android-14.0.0_r0.58
+    # April 2024
+    [kernel_build-redbull]=android-14.0.0_r0.68
+    [kernel_msm-redbull]=android-14.0.0_r0.68
+    [kernel_msm-modules_qcacld-redbull]=android-14.0.0_r0.68
+    [kernel_msm-extra-redbull]=android-14.0.0_r0.68
 
-    # March 2024
-    [kernel_build-gs]=android-14.0.0_r0.51
-    [kernel_gs]=android-14.0.0_r0.51
-    [kernel_google-modules_gpu-gs]=android-14.0.0_r0.51
-    [kernel_google-modules_power_reset-gs]=android-14.0.0_r0.51
-    [kernel_google-modules_wlan_bcmdhd_bcm4389]=android-14.0.0_r0.51
+    # April 2024
+    [kernel_build-gs]=android-14.0.0_r0.66
+    [kernel_gs]=android-14.0.0_r0.66
+    [kernel_google-modules_gpu-gs]=android-14.0.0_r0.66
+    [kernel_google-modules_power_reset-gs]=android-14.0.0_r0.66
+    [kernel_google-modules_wlan_bcmdhd_bcm4389]=android-14.0.0_r0.66
 
-    # March 2024
-    [kernel_devices_google_shusky]=android-14.0.0_r0.57
-    [kernel_google-modules_power_reset-zuma]=android-14.0.0_r0.57
-    [kernel_google-modules_soc_gs]=android-14.0.0_r0.57
-    [kernel_google-modules_wlan_bcmdhd_bcm4398]=android-14.0.0_r0.57
+    # April 2024
+    [kernel_devices_google_shusky]=android-14.0.0_r0.67
+    [kernel_google-modules_power_reset-zuma]=android-14.0.0_r0.67
+    [kernel_google-modules_soc_gs]=android-14.0.0_r0.67
+    [kernel_google-modules_wlan_bcmdhd_bcm4398]=android-14.0.0_r0.67
 )
 
 declare -Ar kernel_tags=(
-    # March 2024
-    [kernel_build-redbull]=android-14.0.0_r0.58
-    [kernel_msm-redbull]=android-14.0.0_r0.58
-    [kernel_msm-modules_qcacld-redbull]=android-14.0.0_r0.58
-    [kernel_msm-extra-redbull]=android-14.0.0_r0.58
+    # April 2024
+    [kernel_build-redbull]=android-14.0.0_r0.68
+    [kernel_msm-redbull]=android-14.0.0_r0.68
+    [kernel_msm-modules_qcacld-redbull]=android-14.0.0_r0.68
+    [kernel_msm-extra-redbull]=android-14.0.0_r0.68
 
-    # March 2024
-    [kernel_build-gs]=android-14.0.0_r0.51
-    [kernel_gs]=android-14.0.0_r0.51
-    [kernel_google-modules_gpu-gs]=android-14.0.0_r0.51
-    [kernel_google-modules_power_reset-gs]=android-14.0.0_r0.51
-    [kernel_google-modules_wlan_bcmdhd_bcm4389]=android-14.0.0_r0.51
+    # April 2024
+    [kernel_build-gs]=android-14.0.0_r0.66
+    [kernel_gs]=android-14.0.0_r0.66
+    [kernel_google-modules_gpu-gs]=android-14.0.0_r0.66
+    [kernel_google-modules_power_reset-gs]=android-14.0.0_r0.66
+    [kernel_google-modules_wlan_bcmdhd_bcm4389]=android-14.0.0_r0.66
 
-    # March 2024
-    [kernel_devices_google_shusky]=android-14.0.0_r0.57
-    [kernel_google-modules_power_reset-zuma]=android-14.0.0_r0.57
-    [kernel_google-modules_soc_gs]=android-14.0.0_r0.57
-    [kernel_google-modules_wlan_bcmdhd_bcm4398]=android-14.0.0_r0.57
+    # April 2024
+    [kernel_devices_google_shusky]=android-14.0.0_r0.67
+    [kernel_google-modules_power_reset-zuma]=android-14.0.0_r0.67
+    [kernel_google-modules_soc_gs]=android-14.0.0_r0.67
+    [kernel_google-modules_wlan_bcmdhd_bcm4398]=android-14.0.0_r0.67
 )
 
 readonly independent=(
