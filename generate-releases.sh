6c610c924b70bf1ca885bed189651f4fc5608378
Daniel Micay <daniel.micay@grapheneos.org>
2024-07-29 01:39:41 -0400

    add generate-releases.sh script

---
+++ generate-releases.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -o errexit -o nounset -o pipefail
+
+source "$(dirname ${BASH_SOURCE[0]})/common.sh"
+
+[[ $# -eq 1 ]] || user_error "expected 1 argument: BUILD_NUMBER"
+
+read -p "Enter key passphrase (empty if none): " -s password
+echo
+export password
+
+chrt -b -p 0 $$
+
+export TMPDIR="${OUT:-$PWD/delta-generation}"
+
+parallel -j4 -q script/release.sh ::: akita husky shiba felix tangorpro lynx cheetah panther bluejay raven oriole barbet redfin bramble ::: $1
