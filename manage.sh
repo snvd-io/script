0e3d60d1666a4e6ef7a9d5a912b10b74a3aba078
Daniel Micay <daniel.micay@grapheneos.org>
2024-08-06 18:05:14 -0400

    Revert "add platform_system_nfc"
    This reverts commit 2a53a68c9ba07a483866a62c917525f8a5deae2b.

--- manage.sh
+++ manage.sh
@@ -99,7 +99,6 @@ readonly aosp_forks=(
     platform_system_logging
     platform_system_librustutils
     platform_system_netd
-    platform_system_nfc
     platform_system_sepolicy
     platform_system_vold
 )
