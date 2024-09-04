3fd7e561331851d82a347fe55cf73d92dcdb9aa3
Daniel Micay <daniel.micay@grapheneos.org>
2024-09-03 21:31:53 -0400

    Android 15

--- common.sh
+++ common.sh
@@ -1,6 +1,6 @@
-readonly branch=14
-readonly aosp_tag_old=android-14.0.0_r67
-readonly aosp_tag=android-14.0.0_r67
+readonly branch=15
+readonly aosp_tag_old=android-15.0.0_r1
+readonly aosp_tag=android-15.0.0_r1
 
 user_error() {
     echo $1 >&2
